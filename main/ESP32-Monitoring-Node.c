9b7900d9ba757aa4e60c594090d0c53372f1e9ad
Mohamed Benmahiddine <mohamedalibenmahiddine@gmail.com>
2026-09-02 11:13:45 +0100

    Add GPIO button interrupt

--- main/ESP32-Monitoring-Node.c
+++ main/ESP32-Monitoring-Node.c
@@ -2,19 +2,48 @@
 #include "freertos/task.h"
 
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 #define LED_GPIO 2
+#define BUTTON_GPIO 0
+
+static const char *TAG = "MONITOR";
+
+static void IRAM_ATTR button_isr_handler(void *arg)
+{
+    ESP_EARLY_LOGI(TAG, "Button interrupt");
+}
 
 void app_main(void)
 {
+    // LED
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_GPIO, 0);
+
+    // Button
+    gpio_config_t button_config = {
+        .pin_bit_mask = (1ULL << BUTTON_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_NEGEDGE,
+    };
+
+    gpio_config(&button_config);
+
+    // Install GPIO interrupt service
+    gpio_install_isr_service(0);
+
+    // Attach ISR to button
+    gpio_isr_handler_add(
+        BUTTON_GPIO,
+        button_isr_handler,
+        NULL);
+
+    ESP_LOGI(TAG, "Monitoring Node started");
 
     while (1)
     {
-        gpio_set_level(LED_GPIO, 1);
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        gpio_set_level(LED_GPIO, 0);
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
